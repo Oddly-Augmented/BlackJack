a7a47fff1e79f202d8676fba171ab296dc277010
Oddly <mairekkgriffiths@gmail.com>
2025-11-30 16:41:26 -0700

    Combined Deck and draw into Shoe class

--- BlackJack.py
+++ BlackJack.py
@@ -6,8 +6,7 @@ RANKS = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 FACE = ['J','Q','K']
 
 
-
-class Deck:
+class Shoe:
     def __init__(self, num_packs=6):
         self.cards = []
         for _ in range(num_packs):
@@ -16,20 +15,17 @@ class Deck:
                     self.cards.append(f"{rank}{suit}")
         random.shuffle(self.cards)
 
-    def size(self):
-        return len(self.cards)
-
-class DrawCards:
-    def __init__(self,deck):
-        self.deck = deck
-    
     def draw(self, n): # draw n cards from the deck and remove them
         drawn = []    
         for _ in range(n):
-            if not self.deck:
+            if not self.cards:
                 break # no more cards
-            drawn.append(self.deck.cards.pop())
+            drawn.append(self.cards.pop())
         return drawn
+    
+    def size(self):
+        return len(self.cards)
+
         
 class Player:
     def __init__(self, name, shoe):
@@ -75,23 +71,33 @@ class Score:
 
         return total
 
-# class Dealer:
-#     def __init__(self):
+class Dealer:
+    def __init__(self, name, shoe):
+        self.name = name
+        self.hand = []
+        self.shoe = shoe
+
+    def dealer_draw(self, n):
+        if self.shoe:
+            self.hand.extend(self.shoe.draw(n))
+        else:
+            raise ValueError("Player has no shoe assigned to draw from")
         
+    def dealer_score(self):
+        return Score(self.hand).get_score()
         
 
 
 if __name__ == "__main__":
-    deck = Deck()
-    shoe = DrawCards(deck)
-    print("Deck size:", deck.size())
+    shoe = Shoe()
+    print("Deck size:", shoe.size())
     print("Draw 4:", shoe.draw(4))
-    print("Deck after draw:", deck.size())
+    print("Deck after draw:", shoe.size())
    
     player = Player(input("Whats your name?: "), shoe)
     player.p_draw(5)
     print(f"{player.name}'s hand: {player.hand}")
-    print(deck.size())
+    print(shoe.size())
     
     print(player.p_score())
   
